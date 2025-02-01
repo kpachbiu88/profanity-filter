c625fba16795e039596f5011645b7a7e2c77f1d1
Konstantin Nikolaev <nikolaev@twozebras.ru>
2025-02-01 20:02:31 +0200

    add swedish language support

--- src/patterns/index.ts
+++ src/patterns/index.ts
@@ -1,9 +1,11 @@
 import patternsRu from './ru';
 import patternsEn from './en';
 import patternsFi from './fi';
+import patternsSv from './sv';
 import replaceRu from '../replaces/ru';
 
 export const ru = patternsRu;
 export const en = patternsEn;
 export const fi = patternsFi;
+export const sv = patternsSv;
 export const replacePatternsRu = replaceRu;
